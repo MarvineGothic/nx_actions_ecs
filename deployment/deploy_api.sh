5659aea754c8939aa1cf2ab8c74b166f195ca9ce
MarvineGothic <marvine.si@gmail.com>
2021-10-08 13:15:33 +0200

    push image

--- deployment/deploy_api.sh
+++ deployment/deploy_api.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-echo "Deploy api"
+SHA=$1
+ECR_REGISTRY=$2
+AWS_REGION="eu-west-1" 
+ECR_REPOSITORY="staging/nx-api"
+ECS_SERVICE="staging-service-backend"
+ECS_CLUSTER="staging-backend"
+ECS_TASK_DEFINITION="deployment/task_definition.json"
+CONTAINER_NAME="staging-api-container"
+
+DOCKER_IMAGE="${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}"
+DOCKER_IMAGE_LATEST="${ECR_REGISTRY}/${ECR_REPOSITORY}:latest"
+
+echo "Build api docker image ${SECRET_KEY} ${REGION}"
+docker build -f ../apps/api/Dockerfile -t .
+docker build -t $DOCKER_IMAGE_LATEST -f ../apps/api/Dockerfile .
+docker tag $DOCKER_IMAGE_LATEST $DOCKER_IMAGE
+docker push $DOCKER_IMAGE_LATEST
+docker push $DOCKER_IMAGE
+
