cb107da29d3f64fa3ace43ea5eb09659e7d32f88
MarvineGothic <marvine.si@gmail.com>
2021-10-08 13:51:33 +0200

    deploy ecr

--- deployment/deploy_api.sh
+++ deployment/deploy_api.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SHA=$1
+IMAGE_TAG=$1
 ECR_REGISTRY="435342033141.dkr.ecr.eu-west-1.amazonaws.com"
 AWS_REGION="eu-west-1" 
 ECR_REPOSITORY="staging/nx-api"
@@ -11,7 +11,7 @@ CONTAINER_NAME="staging-api-container"
 DOCKER_IMAGE="${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}"
 DOCKER_IMAGE_LATEST="${ECR_REGISTRY}/${ECR_REPOSITORY}:latest"
 
-echo "Build api docker image ${SECRET_KEY} ${REGION}"
+echo "Build api docker image"
 docker build -t $DOCKER_IMAGE_LATEST -f ./apps/api/Dockerfile .
 docker tag $DOCKER_IMAGE_LATEST $DOCKER_IMAGE
 docker push $DOCKER_IMAGE_LATEST
