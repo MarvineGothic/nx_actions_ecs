111541272df7bd67904f9c04ac7349c005444123
MarvineGothic <marvine.si@gmail.com>
2021-10-09 13:50:26 +0200

    sfdg

--- deployment/deploy_api.sh
+++ deployment/deploy_api.sh
@@ -28,10 +28,9 @@ OLD_TASK_DEF=$(aws ecs describe-task-definition --task-definition $ECS_TASK_NAME
 NEW_TASK_DEF=$(echo $OLD_TASK_DEF | jq --arg NDI $DOCKER_IMAGE '.taskDefinition.containerDefinitions[0].image=$NDI')
 FINAL_TASK=$(echo $NEW_TASK_DEF | jq '.taskDefinition|{family: .family, volumes: .volumes, containerDefinitions: .containerDefinitions}')
 
-RESULT_REGISTER=$(aws ecs register-task-definition --family $ECS_TASK_NAME --cli-input-json "$(echo $FINAL_TASK)" --memory 2048)
+aws ecs register-task-definition --family $ECS_TASK_NAME --cli-input-json "$(echo $FINAL_TASK)" --memory 2048
 SUCCESS_UPDATE=$(aws ecs update-service --service $SNAME --task-definition $ECS_TASK_NAME --cluster $ECS_CLUSTER)
 
-echo "Register result: ${RESULT_REGISTER}"
 echo "ECS updated: ${SUCCESS_UPDATE}"
 
 
